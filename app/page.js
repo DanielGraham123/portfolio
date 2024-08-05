e0ceea4c9ddc869d5733a18e7f696ce3766d5657
DanielGraham123 <dg.danielboaz@yahoo.com>
2024-08-05 11:45:43 +0100

    feat: Add About component to Home page

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 "use client";
 
+import About from "@/components/About";
 import Hero from "@/components/Hero";
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
 
       <div className="max-w-7xl w-full">
         <Hero />
+        <About />
       </div>
     </main>
   );
